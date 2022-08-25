37a3f03de39ee1cb51f2f488e020e5bfd57bd0ca
MapoDesign <elisaresmini@gmail.com>
2022-08-25 10:54:06 +0200

    deploy

--- src/App.js
+++ src/App.js
@@ -22,6 +22,7 @@ function App() {
       const data = await fetchMovies();
       setMovies(data.movies);
       setTotalCount(data.totalCount);
+      console.log(totalCount)
     }
     callApi();
     return() => {
