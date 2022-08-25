af7d3bff7f3d16dbc0374a761571389d0fc8d723
MapoDesign <elisaresmini@gmail.com>
2022-08-25 15:27:33 +0200

    template: flex justify-content-center and margin

--- src/Components/MovieItem.js
+++ src/Components/MovieItem.js
@@ -1,6 +1,6 @@
 const MovieItem = ({movie}) => {
     return (
-        <div className="card" style={{width: '18rem'}}>
+        <div className="card m-3" style={{width: '18rem'}}>
             <img src={movie.Poster} className="card-img-top" alt={movie.Title} />
             <div className="card-body">
                 <h5 className="card-title">{movie.Title}</h5>
--- src/Components/MovieList.js
+++ src/Components/MovieList.js
@@ -2,7 +2,7 @@ import MovieItem from "./MovieItem"
 
 const MoviesList = ({movies}) => {
     return (
-        <div className="row">
+        <div className="row justify-content-center">
             {
                 movies.map(movie => <MovieItem movie={movie} />)
             }
