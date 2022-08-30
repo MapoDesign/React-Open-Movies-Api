f39b617649a5d0ca871331c7a52cc5284a0b3641
MapoDesign <elisaresmini@gmail.com>
2022-08-30 12:04:05 +0200

    correct text

--- src/Components/MovieItem.js
+++ src/Components/MovieItem.js
@@ -6,7 +6,7 @@ const MovieItem = ({movie, onSelectedMovie}) => {
                 <h5 className="card-title">{movie.Title}</h5>
             </div>
             <div className="card-footer">
-                <button onClick={()=>{onSelectedMovie(movie)}} href={'/movies/' + movie.imdbID} type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">View datail</button>
+                <button onClick={()=>{onSelectedMovie(movie)}} href={'/movies/' + movie.imdbID} type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">View detail</button>
             </div> 
         </div>
     );
